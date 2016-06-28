0ad1205e4dfd757f2b7cb463e61eb8280073ed1b
w_sen <wangsenwd@hotmail.com>
2016-06-28 09:37:44 +0800

    Initial Commit

---
+++ SNTabBarVC-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SNTabBarVC-Swift
+//
+//  Created by wangsen on 16/6/28.
+//  Copyright © 2016年 wangsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
